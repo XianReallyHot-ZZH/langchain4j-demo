6b5e556dbafa43bac2b96d8c8d19990e8ba27264
XianReallyHot-ZZH <741788769@qq.com>
2025-03-15 17:27:56 +0800

    feat:demo-12-文本敏感词检测

---
+++ langchain4j-12sensitive-word/src/main/java/com/example/langchain4j12sensitiveword/Langchain4j12sensitiveWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.langchain4j12sensitiveword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4j12sensitiveWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4j12sensitiveWordApplication.class, args);
+    }
+
+}
